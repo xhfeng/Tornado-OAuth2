def7c459830db080b516c02a1de3db6ad351f073
jiamin <rsj217@gmail.com>
2015-02-04 15:38:22 +0800

    add the access token save and test case

--- app/legacyserver/tests.py
+++ app/legacyserver/tests.py
@@ -3,5 +3,94 @@
 
 __author__ = 'ghost'
 
-client_id = "bb0e26df-7da4-4142-9935-7d9086a089bd"
-client_secret = "9892d9ab-da50-44e9-91c7-15615a7fc60a"
+
+
+import urllib
+import urllib2
+import base64
+import unittest
+import json
+
+
+
+
+
+def post_request(params, url, client_id, client_secret):
+
+    s = "{}:{}".format(client_id, client_secret)
+    b64 = base64.b64encode(s)
+    authorization = 'Basic {}'.format(b64)
+
+    data = urllib.urlencode(params)
+    request = urllib2.Request(url, data)
+    request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    request.add_header('Authorization', authorization)
+    response = urllib2.urlopen(request)
+    if response.code == 200:
+        tokeninfo = response.read()
+        return tokeninfo
+
+def get_request(url, access_token):
+    b64 = base64.b64encode(access_token)
+    authorization = 'Bearer {}'.format(b64)
+
+    request = urllib2.Request(url)
+    request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    request.add_header('Authorization', authorization)
+    response = urllib2.urlopen(request)
+    if response.code == 200:
+        tokeninfo = response.read()
+        return tokeninfo
+
+
+class TestLegacyAuthorize(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://127.0.0.1:8000/legacy/authorize'
+        self.client_id = "bb0e26df-7da4-4142-9935-7d9086a089bd"
+        self.client_secret = "9892d9ab-da50-44e9-91c7-15615a7fc60a"
+        self.params = {
+            'grant_type': 'password',
+            'username': 'admin',
+            'password': '123'
+        }
+
+    def test_unvalid_client(self):
+        error_msg = {'error': 'invalid_client'}
+        self.client_id = 'client_id'
+        result = json.loads(post_request(self.params, self.url, self.client_id, self.client_secret))
+        self.assertDictEqual(error_msg, result)
+
+
+    def test_unvalid_username_and_password(self):
+        error_msg = {'error_description': 'Invalid credentials given.', 'error': 'invalid_grant'}
+        # 错误的用户名和密码
+        self.params.update(username='username')
+        self.params.update(password='password')
+
+        result = json.loads(post_request(self.params, self.url, self.client_id, self.client_secret))
+        self.assertDictEqual(error_msg, result)
+
+
+    def test_unvalid_grant_type(self):
+        error_msg = {'error': 'unsupported_grant_type'}
+        # 错误的 grant_type
+        self.params.update(grant_type='wrong grant_type')
+        result = json.loads(post_request(self.params, self.url, self.client_id, self.client_secret))
+        self.assertDictEqual(error_msg, result)
+
+    def test_unvalid_scopes(self):
+        error_msg = {'error': 'invalid_scope'}
+        self.params.update(scope='commons')
+        result = json.loads(post_request(self.params, self.url, self.client_id, self.client_secret))
+        self.assertDictEqual(error_msg, result)
+
+    def test_default_scopes(self):
+
+        self.params.update(scope='common')
+        result = json.loads(post_request(self.params, self.url, self.client_id, self.client_secret))
+        self.assertTrue('common', result.get('scopes'))
+
+    def test_access_token(self):
+        result = json.loads(post_request(self.params, self.url, self.client_id, self.client_secret))
+        self.assertTrue('access_token' in result.keys())
--- app/legacyserver/validator.py
+++ app/legacyserver/validator.py
@@ -18,83 +18,79 @@ logger = logging.getLogger('oauth')
 
 
 class LegacyValidator(RequestValidator):
-    pass
-    #
-    # def authenticate_client(self, request, *args, **kwargs):
-    #
-    #     logger.info('验证客户端')
-    #     #
-    #     # authorization = request.headers.get('Authorization')
-    #     # access_token_b64 = authorization[6:]
-    #     # client_id, client_secret = base64.b64decode(access_token_b64).split(':')
-    #
-    #     client_id = 'e9bd5e37-2c96-481c-8b4b-86216e504fbd'
-    #     client_secret = '7c27eef1-5071-4e5a-8472-3033f5c9c4e3'
-    #
-    #     try:
-    #         client = Client.objects.get(client_id=client_id, client_secret=client_secret)
-    #     except:
-    #         return False
-    #     setattr(request, 'client', client)
-    #     request._client = client
-    #     return True
-    #
-    # def validate_user(self, username, password, client, request, *args, **kwargs):
-    #     logger.info('验证用户名和密码')
-    #     user = User.objects(username=username, password=password)
-    #     if not user:
-    #         return False
-    #     request._user = user
-    #     return True
-    #
-    # def validate_grant_type(self, client_id, grant_type, client, request, *args, **kwargs):
-    #     logger.info('验证 grant_type 类型')
-    #     try:
-    #         grant_type = dict(map(lambda x: x.split('='), request.body.split('&'))).get('grant_type')
-    #         if grant_type == 'password' or grant_type == 'refresh_token':
-    #             return True
-    #     except:
-    #         return False
-    #
-    # def get_default_scopes(self, client_id, request, *args, **kwargs):
-    #     return request._client.default_scopes
-    #
-    # def validate_scopes(self, client_id, scopes, client, request, *args, **kwargs):
-    #     if not request._client.scopes:
-    #         request._client.scopes = self.get_default_scopes(client_id, request)
-    #         request._client.save()
-    #     return all(map(lambda s: s in request._client.scopes, scopes))
-    #
-    # def save_bearer_token(self, token, request, *args, **kwargs):
-    #     logger.info('生成 token')
-    #     access_token=token.get('access_token')
-    #     refresh_token=token.get('refresh_token')
-    #     expires_in = token.get('expires_in')
-    #     expires_at = genera_expires_time(minutes=expires_in)
-    #     # 授权之后生成 access_token
-    #     if request._user:
-    #         bt = BearerToken.objects(client=request._client, user=request._user[0]).first()
-    #         if bt:
-    #             bt.delete()
-    #         data = dict(
-    #             client = request._client,
-    #             user=request._user[0],
-    #             scopes=request._client.scopes,
-    #             access_token=access_token,
-    #             refresh_token=refresh_token,
-    #             expires_at=expires_at
-    #         )
-    #         try:
-    #             bt = BearerToken(**data)
-    #             bt.save()
-    #         except Exception, e:
-    #             print e
-    #     # refresh 更新保存 access_token
-    #     else:
-    #         request._bearertoken.expires_at = expires_at
-    #         request._bearertoken.access_token = token.get('access_token')
-    #         request._bearertoken.refresh_token = token.get('refresh_token')
-    #         request._bearertoken.save()
+
+    def authenticate_client(self, request, *args, **kwargs):
+
+        logger.info('验证客户端')
+
+        authorization = request.headers.get('Authorization')
+        access_token_b64 = authorization[6:]
+        client_id, client_secret = base64.b64decode(access_token_b64).split(':')
+
+        try:
+            client = Client.objects.get(client_id=client_id, client_secret=client_secret)
+        except:
+            return False
+        setattr(request, 'client', client)
+        request._client = client
+        return True
+
+    def validate_user(self, username, password, client, request, *args, **kwargs):
+        logger.info('验证用户名和密码')
+        user = User.objects(username=username, password=password)
+        if not user:
+            return False
+        request._user = user
+        return True
+
+    def validate_grant_type(self, client_id, grant_type, client, request, *args, **kwargs):
+        logger.info('验证 grant_type 类型')
+        try:
+            grant_type = dict(map(lambda x: x.split('='), request.body.split('&'))).get('grant_type')
+            if grant_type == 'password' or grant_type == 'refresh_token':
+                return True
+        except:
+            return False
+
+    def get_default_scopes(self, client_id, request, *args, **kwargs):
+        return request._client.default_scopes
+
+    def validate_scopes(self, client_id, scopes, client, request, *args, **kwargs):
+        if not request._client.scopes:
+            request._client.scopes = self.get_default_scopes(client_id, request)
+            request._client.save()
+        return all(map(lambda s: s in request._client.scopes, scopes))
+
+    def save_bearer_token(self, token, request, *args, **kwargs):
+        logger.info('生成 token')
+        access_token=token.get('access_token')
+        refresh_token=token.get('refresh_token')
+        expires_in = token.get('expires_in')
+        expires_at = genera_expires_time(minutes=expires_in)
+        # 授权之后生成 access_token
+        if request._user:
+            bt = BearerToken.objects(client=request._client, user=request._user[0]).first()
+            if bt:
+                bt.delete()
+            data = dict(
+                client = request._client,
+                user=request._user[0],
+                scopes=request._client.scopes,
+                access_token=access_token,
+                refresh_token=refresh_token,
+                expires_at=expires_at
+            )
+            try:
+                bt = BearerToken(**data)
+                bt.save()
+            except Exception, e:
+                print e
+        # refresh 更新保存 access_token
+        else:
+            request._bearertoken.expires_at = expires_at
+            request._bearertoken.access_token = token.get('access_token')
+            request._bearertoken.refresh_token = token.get('refresh_token')
+            request._bearertoken.save()
     #
     # def validate_refresh_token(self, refresh_token, client, request, *args, **kwargs):
     #
